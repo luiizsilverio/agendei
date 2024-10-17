6a10ab98311cc571ea0a07e7bb7b0ad0a8fbab0c
luiizsilverio <luiiz.silverio@gmail.com>
2024-10-17 19:51:17 -0300

    Aula 02 OK

--- agendei-mobile/App.js
+++ agendei-mobile/App.js
@@ -2,15 +2,12 @@ import { StatusBar } from 'react-native';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from './src/routes';
-import Services from './src/screens/services';
-import Schedule from './src/screens/schedule';
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
-        {/* <Routes /> */}
-        <Schedule />
+        <Routes />
       </NavigationContainer>
       <StatusBar style="auto" />
     </>
--- agendei-mobile/src/screens/home/home.style.js
+++ agendei-mobile/src/screens/home/home.style.js
@@ -14,5 +14,30 @@ export const styles = StyleSheet.create({
     fontSize: FONT_SIZE.md,
     color: COLORS.gray1,
     marginBottom: 16,
-  }
+  },
+  containerLogo: {
+    alignItems: "center"
+  },
+  image: {
+    maxWidth: 140,
+    objectFit: "contain"
+    // width: 100,
+    // height: 23
+  },
+  footer: {
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  formContainer: {
+    rowGap: 14
+  },
+  input: {
+    backgroundColor: COLORS.gray5,
+    padding: 10,
+    borderRadius: 6
+  },
+  link: {
+    color: COLORS.blue,
+
+  },
 })
