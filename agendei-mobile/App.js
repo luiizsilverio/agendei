77b8c72d763947fddc10f6c7328ee8f49c74a9e9
luiizsilverio <luiiz.silverio@gmail.com>
2024-10-17 20:00:12 -0300

    Aula 02 OK

--- agendei-mobile/App.js
+++ agendei-mobile/App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'react-native';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import Routes from './src/routes';
+import Services from './src/screens/services';
+import Schedule from './src/screens/schedule';
 
 export default function App() {
   return (
     <>
       <NavigationContainer>
         <Routes />
+        {/* <Services /> */}
       </NavigationContainer>
       <StatusBar style="auto" />
     </>
