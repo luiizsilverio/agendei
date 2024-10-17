2e281ae94ba49771a467bfb24a5bb9f15f47913e
luiizsilverio <luiiz.silverio@gmail.com>
2024-10-17 19:38:38 -0300

    Aula 03 Ok

--- agendei-mobile/src/screens/schedule/index.jsx
+++ agendei-mobile/src/screens/schedule/index.jsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import { Text, View } from "react-native";
 import { Calendar, LocaleConfig } from "react-native-calendars";
+import { Picker } from "@react-native-picker/picker";
 import { styles } from "./schedule.style";
 import { ptBR } from "../../constants/calendar";
+import Button from "../../components/button";
 
 LocaleConfig.locales["pt-br"] = ptBR;
 LocaleConfig.defaultLocale = 'pt-br';
 
 export default function Schedule() {
+  const hoje = new Date().toISOString().slice(0, 10);
+  const [data, setData] = useState(hoje);
+  const [horario, setHorario] = useState("");
 
   return (
     <View  style={styles.container}>
-      <Calendar theme={styles.theme} />
+      <Calendar 
+        theme={styles.calendar} 
+        onDayPress={(day) => setData(day.dateString)}
+        markedDates={{
+          [data]: { selected: true },
+        }}
+        minDate={new Date().toDateString()}
+      />
+
+      <Text style={styles.horario}>Horário</Text>
+
+      <Picker 
+        selectedValue={horario} 
+        onValueChange={(itemValue, itemIndex) => {
+          setHorario(itemValue);
+      }}>
+        <Picker.Item label="09:00" value="09:00" />
+        <Picker.Item label="09:30" value="09:30" />
+        <Picker.Item label="10:00" value="10:00" />
+      </Picker>
+  
+      <View style={styles.btnContainer}>
+        <Button text="Confirmar Agendamento" />
+      </View>
     </View>
   )
 }
--- agendei-mobile/src/screens/schedule/schedule.style.js
+++ agendei-mobile/src/screens/schedule/schedule.style.js
@@ -3,7 +3,8 @@ import { COLORS, FONT_SIZE } from "../../constants/theme";
 
 export const styles = StyleSheet.create({  
   container: {
-    flex: 1,    
+    // flex: 1,    
+    height: "100%",
     backgroundColor: COLORS.white,
     paddingHorizontal: 20,
   },
@@ -12,10 +13,27 @@ export const styles = StyleSheet.create({
     color: COLORS.gray1,
     marginBottom: 16,
   },
-  theme: {
-    todayTextColor: COLORS.red,
+  calendar: {
+    todayTextColor: COLORS.blue,
+    // todayBackgroundColor: COLORS.red,
     selectedDayBackgroundColor: COLORS.blue,
     selectedDayTextColor: COLORS.white,
     arrowColor: COLORS.blue
+  },
+  horario: {
+    fontSize: FONT_SIZE.lg,
+    fontWeight:  "bold",
+    color: COLORS.gray2,
+    borderTopWidth: 1,
+    borderTopColor: COLORS.gray5,
+    marginTop: 18,
+    paddingTop: 20,
+
+  },
+  btnContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    marginBottom: 30,
+    paddingHorizontal: 8
   }
 })
